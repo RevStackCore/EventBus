e7298e887d19d51fd198e745734b66377686d2f8
meanstream <david@misonline.biz>
2017-11-19 13:34:59 -0500

    release v1.0.3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EventBus
+
+[![Build status](https://ci.appveyor.com/api/projects/status/v2a3tkkbq9lyylh7?svg=true)](https://ci.appveyor.com/project/tachyon1337/eventbus)
---
+++ RevStackCore.EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace RevStackCore.EventBus
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler 
+        where TIntegrationEvent: IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
